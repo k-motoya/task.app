279051795ef3813b3c440a2ca6629dd94ae036c8
kenta motoya <kenta@kentanoMacBook-Pro.local>
2019-08-24 16:44:13 +0900

    Task Userの紐付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_050846) do
+ActiveRecord::Schema.define(version: 2019_08_24_073814) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
