58792ab1cf204ab94047ac0d18ee60de50e79d34
kenta motoya <kenta@kentanoMacBook-Pro.local>
2019-08-28 12:29:49 +0900

    他のユーザーが作成したタスクが表示されないことの確認

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -18,9 +18,26 @@ describe 'タスク管理機能', type: :system do
         click_button 'ログインする'
       end
 
-      it 'ユーザーAが作成したタスクが表示される' do
-          #作成済みのタスクの名称が画面上に表示されていることを確認
-          expect(page).to have_content '最初のタスク'
+      it 'ユーザーAが作成たタスクの表示' do
+        #ユーザーAが作成したタスクが画面上に表示されていることを確認
+        expect(page).to have_content '最初のタスク'
+      end
+
+      context 'ユーザーBがログインしている時' do
+        before do
+          #ユーザーBを作成しておく
+          FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
+          #ユーザーBでログインする
+          visit login_path
+          fill_in 'メールアドレス', with: 'b@example.com'
+          fill_in 'パスワード', with: 'password'
+          click_button 'ログインする'
+        end
+
+        it 'ユーザーAが作成したタスクが作成されない' do
+          #ユーザーAが作成したタスクが画面上に表示されていないことを確認
+          expect(page).to have_no_content '最初のタスク'
+        end
       end
     end
   end
