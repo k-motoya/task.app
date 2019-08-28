18579a252d9ad49c578a6ffc85258b08649bb4a1
kenta motoya <kenta@kentanoMacBook-Pro.local>
2019-08-28 13:32:42 +0900

    詳細表示のSpec追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,27 +4,28 @@ describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')}
     let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')}
+    let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a)}
 
     before do
       # #ユーザーAを作成しておく
       # user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
       #作成者がユーザーAであるタスクを作成しておく
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-
+      # FactoryBot.create(:task, name: '最初のタスク', user: user_a)
       visit login_path
       fill_in 'メールアドレス', with: login_user.email
       fill_in 'パスワード', with: login_user.password
       click_button 'ログインする'
     end
 
-    context 'ユーザAがログインしている時' do
-      let(:login_user) {user_a}
+    describe '一覧表示機能' do
+      context 'ユーザAがログインしている時' do
+        let(:login_user) {user_a}
 
-      it 'ユーザーAが作成たタスクの表示' do
-          #ユーザーAが作成したタスクが画面上に表示されていることを確認
-        expect(page).to have_content '最初のタスク'
+        it 'ユーザーAが作成たタスクの表示' do
+            #ユーザーAが作成したタスクが画面上に表示されていることを確認
+          expect(page).to have_content '最初のタスク'
+        end
       end
-    end
 
       context 'ユーザーBがログインしている時' do
         let(:login_user) {user_b}
@@ -43,4 +44,19 @@ describe 'タスク管理機能', type: :system do
         end
       end
     end
+
+    describe '詳細表示機能' do
+      context 'ユーザーAがログインしている時' do
+        let(:login_user) { user_a }
+
+        before do
+          visit task_path(task_a)
+        end
+
+        it 'ユーザーAが作成したタスクが表示される' do
+          expect(page).to have_content '最初のタスク'
+        end
+      end
+    end
   end
+end
